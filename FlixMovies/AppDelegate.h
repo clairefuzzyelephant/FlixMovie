614b723d494a381c256e2e32edab7234c44452a5
Claire Cheng <clairec@fb.com>
2019-06-26 11:28:39 -0700

    Initial Commit

---
+++ FlixMovies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlixMovies
+//
+//  Created by clairec on 6/26/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlixMoviesTests/FlixMoviesTests.m
@@ -0,0 +1,37 @@
+//
+//  FlixMoviesTests.m
+//  FlixMoviesTests
+//
+//  Created by clairec on 6/26/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FlixMoviesTests : XCTestCase
+
+@end
+
+@implementation FlixMoviesTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
